9732f2ccd690c5a4c1fefe469333b5853c934267
Макс Дороболюк <dmv_01@mail.ru>
2021-02-12 17:36:11 +0600

    Changed algorithm

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace ConsoleApp7
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
             List<int> startStatement = new List<int>();
             string inputStatement = Console.ReadLine();
             inputStatement = inputStatement.Replace(" ", "");
@@ -25,41 +24,64 @@ namespace ConsoleApp7
             int average =(int)chipsOnSites.Average();
             while (!chipsOnSites.All(x=>x==average))
             {
-                int indexOfTarget = chipsOnSites.IndexOf(chipsOnSites.Where(x=>x>average).Min());
-                for (int wave = 1; wave <= chipsOnSites.Count/2&&chipsOnSites[indexOfTarget]>average; wave++)
+                int indexOfTarget = chipsOnSites.IndexOf(chipsOnSites.Where(x=>x<average).Max());
+                StateViewModified(chipsOnSites, indexOfTarget);
+                int leftWeight=0, rightWeight=0;
+                for (int i = 1; i <= chipsOnSites.Count/2; i++)
                 {
-                    int leftIndexOfSubTarget, rightIndexOfSubTarget;
-                    if (indexOfTarget - wave < 0)
-                        leftIndexOfSubTarget = chipsOnSites.Count - indexOfTarget - wave;
-                    else
-                        leftIndexOfSubTarget = indexOfTarget - wave;
-                    if (indexOfTarget + wave >= chipsOnSites.Count)
-                        rightIndexOfSubTarget = 0 + wave - (chipsOnSites.Count - indexOfTarget);
-                    else
-                        rightIndexOfSubTarget = indexOfTarget + wave;
-                    while (chipsOnSites[indexOfTarget]>average&&(chipsOnSites[rightIndexOfSubTarget]<average||chipsOnSites[leftIndexOfSubTarget]<average))
-                    {
-                        if (chipsOnSites[leftIndexOfSubTarget]<=chipsOnSites[rightIndexOfSubTarget])
-                            chipsOnSites[leftIndexOfSubTarget]++;
-                        else
-                            chipsOnSites[rightIndexOfSubTarget]++;
-                        chipsOnSites[indexOfTarget]--;
-                        countOfChanges += wave;
-                        StateView(chipsOnSites);
-                    }
+                    int leftIndex=indexOfTarget-i<0?(chipsOnSites.Count-i+indexOfTarget):(indexOfTarget-i),
+                        rightIndex=indexOfTarget+i>=chipsOnSites.Count?(i-(chipsOnSites.Count-indexOfTarget)):(indexOfTarget+i);
+                    leftWeight += chipsOnSites[leftIndex];
+                    rightWeight += chipsOnSites[rightIndex];
                 }
+                for (int wave = 1; wave <= chipsOnSites.Count/2; wave++)
+                {
+                    int leftIndex = indexOfTarget - wave < 0 ? (chipsOnSites.Count - wave + indexOfTarget) : (indexOfTarget - wave),
+                        rightIndex = indexOfTarget + wave >= chipsOnSites.Count ? (wave - (chipsOnSites.Count - indexOfTarget)) : (indexOfTarget + wave);
+                    if (rightWeight>=leftWeight)
+                    {
+                        if (chipsOnSites[rightIndex]>average)
+                        {
+                            chipsOnSites[rightIndex]--;
+                            chipsOnSites[indexOfTarget]++;
+                            countOfChanges += wave;
+                            break;
+                        }
+                    }
+                    else if (chipsOnSites[leftIndex]>average)
+                    {
+                        chipsOnSites[leftIndex]--;
+                        chipsOnSites[indexOfTarget]++;
+                        countOfChanges += wave;
+                        break;
+                    }
 
-
+                }
             }
             Console.WriteLine("Итоговое состояние");
             StateView(chipsOnSites);
             return countOfChanges;
         }
+        static void StateViewModified(List<int> statements,int pos)
+        {
+            for (int i = 0; i < statements.Count; i++)
+            {
+                if (i==pos)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(statements[i]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(", ");
+                }
+                else Console.Write(statements[i]+", ");
+            }
+            Console.WriteLine();
+        }
         static void StateView(List<int> statements)
         {
             foreach (var item in statements)
             {
-                Console.Write($"{item},");
+                Console.Write($"{item}, ");
             }
             Console.WriteLine();
         }
